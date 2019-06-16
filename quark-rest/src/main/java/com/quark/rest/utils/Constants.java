7ff326ec5f61ec587d649c462da02334191b0614
liudeyu <liudeyu@bytedance.com>
2019-06-16 20:36:36 +0800

    file constants change

--- quark-rest/src/main/java/com/quark/rest/utils/Constants.java
+++ quark-rest/src/main/java/com/quark/rest/utils/Constants.java
@@ -1,12 +1,26 @@
 package com.quark.rest.utils;
 
+import java.io.File;
+
 /**
  * @Author LHR
  * Create By 2017/8/22
  */
 public class Constants {
-    //Upload常量
-    public static final String UPLOAD_PATH = "D:\\home\\images\\upload\\";
-    public static final String STATIC_URL ="http://127.0.0.1/images/upload/";
+
+    public static final String RELEATIVE_UPLOAD_PATH = "images/upload";
+    //Upload常
+    public static String UPLOAD_PATH = "";
+    public static final String STATIC_URL = "http://127.0.0.1/" + RELEATIVE_UPLOAD_PATH;
+
+    static {
+        UPLOAD_PATH = System.getProperty("user.home");
+        if (UPLOAD_PATH.endsWith(File.separator)) {
+            UPLOAD_PATH = UPLOAD_PATH = File.separator;
+        }
+        UPLOAD_PATH = UPLOAD_PATH + RELEATIVE_UPLOAD_PATH;
+
+
+    }
 
 }
